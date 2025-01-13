8a223f1e85a0e853c998617058fa925681d586c7
tayier <ntparis9@gmail.com>
2025-01-13 21:57:01 +0100

    fix bugs router

--- src/presentation_Layer/hooks/useImageCarousel.jsx
+++ src/presentation_Layer/hooks/useImageCarousel.jsx
@@ -8,26 +8,12 @@ export const useImageCarousel = (cover, pictures) => {
   const currentIndex = useRef(0);
 
   const turnImgHandler = useCallback((arg) => {
-     currentIndex.current = sliderService(currentIndex,pictures, arg);
-    setImg(pictures[ currentIndex.current]);
-    currentImgNumber.current =  currentIndex.current + 1;
+    currentIndex.current = sliderService(currentIndex, pictures, arg);
+    setImg(pictures[currentIndex.current]);
+    currentImgNumber.current = currentIndex.current + 1;
   });
 
   return { turnImgHandler, imag, isSingleImg, currentImgNumber };
 };
 
-// (arg) => {
-//   const maxIndex = pictures.length - 1;
 
-//   index.current += arg;
-//   if (maxIndex == 0) {
-//     isSingleImg.current = true;
-//   } else if (index.current > maxIndex) {
-//     index.current = 0;
-//   } else if (index.current < 0) {
-//     index.current = maxIndex;
-//   }
-//   setImg(pictures[index.current]);
-//   currentImgNumber.current = index.current + 1;
-//   return "ok";
-// }
